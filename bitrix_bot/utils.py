dcea4dd2fb48e06444448368d09de46d56f1739f
Neggergert <oleggergert@outlook.com>
2025-05-18 18:03:51 +0500

    Fix get_url return and update handlers

--- bitrix_bot/utils.py
+++ bitrix_bot/utils.py
@@ -15,10 +15,9 @@ def extract_mention_username(message):
 async def get_url_by_type(chat_id: int, chat_type: str,
                           context: ContextTypes.DEFAULT_TYPE):
     if chat_type == "private":
-        user_row = get_url(chat_id)
-        return user_row
-    elif chat_type == "group" or chat_type == "supergroup":
-        bitrix_url, group_id = await get_uinfo_from_admins(chat_id, context)
+        return get_url(chat_id)
+    elif chat_type in ("group", "supergroup"):
+        bitrix_url, _ = await get_uinfo_from_admins(chat_id, context)
         return bitrix_url
     return None
 
